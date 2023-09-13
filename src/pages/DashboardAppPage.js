7026c404cec7a74836b1d4b1473eafdd97169e1c
Julio Benavides <jbenavidesji@uni-osnabrueck.de>
2023-09-13 12:25:27 +0200

    new sections for dashboard

--- src/pages/DashboardAppPage.js
+++ src/pages/DashboardAppPage.js
@@ -1,8 +1,9 @@
 import { Helmet } from 'react-helmet-async';
 import { faker } from '@faker-js/faker';
+import React, {useEffect, useState} from "react";
 // @mui
 import { useTheme } from '@mui/material/styles';
-import { Grid, Container, Typography } from '@mui/material';
+import {Grid, Container, Typography, Divider} from '@mui/material';
 // components
 import Iconify from '../components/iconify';
 // sections
@@ -17,23 +18,84 @@ import {
   AppCurrentSubject,
   AppConversionRates,
 } from '../sections/@dashboard/app';
+import AppConsumeCurrentSummary from "../sections/@dashboard/app/AppConsumeCurrentSummary";
+import {getAllHints} from "../services/HintsServices";
+import {getAllConsumeCurrent} from "../services/ConsumeCurrentServices";
+import {getAllSavings} from "../services/SavingsServices";
+import AppSavingsChart from "../sections/@dashboard/app/AppSavingsChart";
 
 // ----------------------------------------------------------------------
 
 export default function DashboardAppPage() {
-  const theme = useTheme();
 
-  return (
-    <>
-      <Helmet>
-        <title> Dashboard </title>
-      </Helmet>
+    const [hints, setHints] = useState([])
+    const [consumeCurrentValues, setConsumeCurrent] = useState([])
+    const [savings, setSavings] = useState([])
+    const [savingAmountData, setSavingAmountData] = useState([]);
+    const [savingDateData, setSavingDateData] = useState([]);
 
-      <Container maxWidth="xl">
-        <Typography variant="h4" sx={{ mb: 5 }}>
-          Smart Heating
-        </Typography>
-      </Container>
+    const theme = useTheme();
+
+
+    useEffect(() => {
+        getAllConsumeCurrent()
+          .then(consumeCurrentValues => {
+              setConsumeCurrent(consumeCurrentValues);
+          });
+
+        getAllSavings()
+            .then(savings => {
+                setSavings(savings);
+                const savingsAmount = savings.map(saving => saving.amount_saving);
+                const savingsDate = savings.map(saving => saving.date_formatted_saving);
+                setSavingAmountData(savingsAmount);
+                setSavingDateData(savingsDate);
+            })
+        },[]
+    )
+    return (
+        <>
+            <Helmet>
+                <title> Dashboard </title>
+            </Helmet>
+
+            <Container maxWidth="xl">
+                <Typography variant="h3" sx={{ mb: 5 }}>
+                  Smart Heating
+                </Typography>
+                <Typography variant="h4" sx={{ mb: 5 }}>
+                  Übersicht
+                </Typography>
+
+                <Grid container spacing={3}>
+                    <Grid item xs={12} md={6} lg={12}>
+                        <AppSavingsChart
+                            title="Erspannise im Jahr 2022"
+                            subheader="(+43%) mehr als letztes Jahr"
+                            chartLabels={savingDateData}
+                            chartData={[
+                                {
+                                    name: 'Euro',
+                                    type: 'area',
+                                    fill: 'gradient',
+                                    data: savingAmountData,
+                                },
+                            ]}
+                        />
+                    </Grid>
+
+                    <Grid item xs={12} sm={6} md={12}>
+                        {consumeCurrentValues.map(consumeCurrent => (
+                            <AppConsumeCurrentSummary key={consumeCurrent.id_cc} title={consumeCurrent.name_room_cc} total={consumeCurrent.current_temperature_cc} color={consumeCurrent.state_color_cc} icon={'ant-design:bug-filled'} />
+                        ))}
+                    </Grid>
+
+
+
+                </Grid>
+
+
+          </Container>
     </>
   );
 }
---
+++ src/sections/@dashboard/app/index.js
@@ -0,0 +1,2 @@
+export { default as AppConsumeCurrentSummary } from './AppConsumeCurrentSummary';
+export { default as AppSavingsChart } from './AppSavingsChart';
