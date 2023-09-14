b2133c02640dea5817211875f7d0461829ffb9f7
Julio Benavides <jbenavidesji@uni-osnabrueck.de>
2023-09-14 16:07:50 +0200

    export sections from dashboard

--- src/sections/@dashboard/app/index.js
+++ src/sections/@dashboard/app/index.js
@@ -1,2 +1,4 @@
 export { default as AppConsumeCurrentSummary } from './AppConsumeCurrentSummary';
 export { default as AppSavingsChart } from './AppSavingsChart';
+export { default as AppSavingsTotalCard } from './AppSavingsTotalCard';
+export { default as AppConsumeMeasurementsChart } from './AppConsumeMeasurementsChart';
