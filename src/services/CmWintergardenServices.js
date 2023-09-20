f0da0080bfe643c56d250b8eff44799f2b474125
Julio Benavides <jbenavidesji@uni-osnabrueck.de>
2023-09-20 15:17:02 +0200

    FIX: false variable sql nam

--- src/services/CmWintergardenServices.js
+++ src/services/CmWintergardenServices.js
@@ -1,6 +1,6 @@
 export async function getAllCmWintergarten() {
     try {
-        const response = await fetch("http://localhost:5000/cm-wintergarten");
+        const response = await fetch("http://localhost:5000/cm-wintergarden");
         return await response.json();
     } catch (err) {
         return console.error(err.message);
